9f49f557017b520ba8d5caadb5728618098ca209
DaFungaJunga <shomari.simpson@uoit.net>
2019-03-18 15:26:08 -0400

    Update ANN.py

--- Project3/ANN.py
+++ Project3/ANN.py
@@ -8,12 +8,11 @@ import matplotlib.cm as cm
 
 
 class DigitBitmap:
-    def __init__(self, num):
-        self.num = num
+    def __init__(self):
         self.bitmap = np.empty([9, 5])
 
-    def initialize(self):
-        if self.num == 1:
+    def initialize(self,num):
+        if num == 1:
             arr = np.array([[0, 0, 1, 0, 0],
                             [0, 1, 1, 0, 0],
                             [1, 0, 1, 0, 0],
@@ -24,7 +23,7 @@ class DigitBitmap:
                             [0, 0, 1, 0, 0],
                             [0, 0, 1, 0, 0]])
             self.bitmap = arr
-        if self.num == 2:
+        if num == 2:
             arr = np.array([[0, 1, 1, 1, 0],
                             [1, 0, 0, 0, 1],
                             [1, 0, 0, 0, 1],
@@ -35,7 +34,7 @@ class DigitBitmap:
                             [1, 0, 0, 0, 0],
                             [1, 1, 1, 1, 1]])
             self.bitmap = arr
-        if self.num == 3:
+        if num == 3:
             arr = np.array([[0, 1, 1, 1, 0],
                             [1, 0, 0, 0, 1],
                             [0, 0, 0, 0, 1],
@@ -46,7 +45,7 @@ class DigitBitmap:
                             [1, 0, 0, 0, 1],
                             [0, 1, 1, 1, 0]])
             self.bitmap = arr
-        if self.num == 4:
+        if num == 4:
             arr = np.array([[0, 0, 0, 1, 0],
                             [0, 0, 1, 1, 0],
                             [0, 0, 1, 1, 0],
@@ -57,7 +56,7 @@ class DigitBitmap:
                             [0, 0, 0, 1, 0],
                             [0, 0, 0, 1, 0]])
             self.bitmap = arr
-        if self.num == 5:
+        if num == 5:
             arr = np.array([[1, 1, 1, 1, 1],
                             [1, 0, 0, 0, 0],
                             [1, 0, 0, 0, 0],
@@ -68,7 +67,7 @@ class DigitBitmap:
                             [1, 0, 0, 0, 1],
                             [0, 1, 1, 1, 0]])
             self.bitmap = arr
-        if self.num == 6:
+        if num == 6:
             arr = np.array([[0, 1, 1, 1, 0],
                             [1, 0, 0, 0, 1],
                             [1, 0, 0, 0, 0],
@@ -79,7 +78,7 @@ class DigitBitmap:
                             [1, 0, 0, 0, 1],
                             [0, 1, 1, 1, 0]])
             self.bitmap = arr
-        if self.num == 7:
+        if num == 7:
             arr = np.array([[1, 1, 1, 1, 1],
                             [0, 0, 0, 0, 1],
                             [0, 0, 0, 1, 0],
@@ -90,7 +89,7 @@ class DigitBitmap:
                             [0, 1, 0, 0, 0],
                             [0, 1, 0, 0, 0]])
             self.bitmap = arr
-        if self.num == 8:
+        if num == 8:
             arr = np.array([[0, 1, 1, 1, 0],
                             [1, 0, 0, 0, 1],
                             [1, 0, 0, 0, 1],
@@ -101,7 +100,7 @@ class DigitBitmap:
                             [1, 0, 0, 0, 1],
                             [0, 1, 1, 1, 0]])
             self.bitmap = arr
-        if self.num == 9:
+        if num == 9:
             arr = np.array([[0, 1, 1, 1, 0],
                             [1, 0, 0, 0, 1],
                             [1, 0, 0, 0, 1],
@@ -112,7 +111,7 @@ class DigitBitmap:
                             [1, 0, 0, 0, 1],
                             [0, 1, 1, 1, 0]])
             self.bitmap = arr
-        if self.num == 0:
+        if num == 0:
             arr = np.array([[0, 1, 1, 1, 0],
                             [1, 0, 0, 0, 1],
                             [1, 0, 0, 0, 1],
@@ -132,7 +131,8 @@ class Perceptron:
     def initializeWeights(self):
         for i in range(9):
             for j in range(5):
-                self.weight[i][j] = random.uniform(0, 1)
+                #self.weight[i][j] = random.uniform(-2.4/(9*5), 2.4/(9*5))
+                self.weight[i][j] = random.uniform(-0.5, 0.5)
 
     def updateWeight(self, i, j, inp, error, learningRate):
         self.weight[i][j] += learningRate * inp * error
@@ -140,145 +140,93 @@ class Perceptron:
 
 class ANN:
 
-    def __init__(self, epochs, number, perceptrons):
+    def __init__(self, epochs):
         self.output = np.empty(10)
+        self.workingOutput = np.empty(10)
         self.desiredOutput = np.empty(10)
         self.epochs = epochs
         self.sumSquareError = np.empty(epochs)
-        self.number = number
         self.threshold = 0.2
-        self.learningRate = 0.2
-        self.perceptronNum = perceptrons
-        self.hiddenPerceptrons = [Perceptron() for i in range(perceptrons)]
-        self.workingBitmap = DigitBitmap(self.number)
+        self.learningRate = 0.1
+        self.perceptronNum = 10
+        self.hiddenPerceptrons = [Perceptron() for i in range(10)]
+        self.workingBitmap = DigitBitmap()
 
     def initialization(self):
-        self.workingBitmap.initialize()
-
         for p in range(self.perceptronNum):
             self.hiddenPerceptrons[p].initializeWeights()
 
-        if self.number == 0:
+    def currentDesiredOutput(self, num):
+        self.workingBitmap.initialize(num)
+
+        if num == 0:
             self.desiredOutput = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        if self.number == 1:
+        if num == 1:
             self.desiredOutput = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-        if self.number == 2:
+        if num == 2:
             self.desiredOutput = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
-        if self.number == 3:
+        if num == 3:
             self.desiredOutput = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-        if self.number == 4:
+        if num == 4:
             self.desiredOutput = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-        if self.number == 5:
+        if num == 5:
             self.desiredOutput = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
-        if self.number == 6:
+        if num == 6:
             self.desiredOutput = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-        if self.number == 7:
+        if num == 7:
             self.desiredOutput = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
-        if self.number == 8:
+        if num == 8:
             self.desiredOutput = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-        if self.number == 9:
+        if num == 9:
             self.desiredOutput = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 
     def run(self):
-        count = 0
-        print("For Bitmap: " + str(self.number))
+        for o in range(10):
+            count = 0
+            self.currentDesiredOutput(o)
 
-        while count < self.epochs:
+            while count < self.epochs:
 
-            for p in range(self.perceptronNum):
-                self.output[p] = 0
-
-                for i in range(9):
-                    for j in range(5):
-                        self.output[p] += self.workingBitmap.bitmap[i][j] * self.hiddenPerceptrons[p].weight[i][j]
-
-
-
-                # adjust threshold value when testing for better results
-                self.output[p] -= self.threshold
-                if self.output[p] >= 0:
-                    self.output[p] = 1
-                else:
+                for p in range(self.perceptronNum):
                     self.output[p] = 0
+                    self.workingOutput[p] = 0
+                    for i in range(9):
+                        for j in range(5):
+                            self.workingOutput[p] += self.workingBitmap.bitmap[i][j] * self.hiddenPerceptrons[p].weight[i][j]
+
+                    # adjust threshold value when testing for better results
+                    #print(str(self.output[p]))
+                    self.workingOutput[p] -= self.threshold
+                    #print(str(self.workingOutput[p]))
+                    if self.workingOutput[p] >= 0:
+                        self.output[p] = 1
+                    else:
+                        self.output[p] = 0
+
+                mean = 0
+
+                for p in range(self.perceptronNum):
+                    mean += self.desiredOutput[p] - self.output[p]
+                    for i in range(9):
+                        for j in range(5):
+                            self.hiddenPerceptrons[p].updateWeight(i, j, self.workingBitmap.bitmap[i][j],
+                                                                   (self.desiredOutput[p] - self.output[p]),
+                                                                   self.learningRate)
+
+                mean = mean / self.perceptronNum
+
+                for p in range(self.perceptronNum):
+                    self.sumSquareError[count] += ((self.desiredOutput[p] - self.output[p]) - mean) ** 2
+                print(
+                    "Epoch: " + str(count) + " Desired Output: " + str(self.desiredOutput) + " Current Output: " + str(
+                        self.output) + "Sum Squared Error: " + str(self.sumSquareError[count]))
+                if self.sumSquareError[count] == 0:
+                    print("Solution Found, moving to next number")
+                    break
+                count += 1
 
 
-            mean = 0
-
-            for p in range(self.perceptronNum):
-                mean += self.desiredOutput[p] - self.output[p]
-                for i in range(9):
-                    for j in range(5):
-                        self.hiddenPerceptrons[p].updateWeight(i, j, self.workingBitmap.bitmap[i][j], (self.desiredOutput[p] - self.output[p]), self.learningRate)
-
-            mean = mean / self.perceptronNum
-
-            for p in range(self.perceptronNum):
-                self.sumSquareError[count] += ((self.desiredOutput[p] - self.output[p]) - mean) ** 2
-
-            count += 1
-
-            print("Epoch: " + str(count) + " Desired Output: " + str(self.desiredOutput) + " Current Output: " + str(
-                self.output))
-
-        plt.figure(self.number)
-        plt.plot(self.sumSquareError)
-        plt.ylabel('Sum Square Error')
-        plt.xlabel('Epochs')
-        plt.title('Sum Square Error vs. Epochs (' + str(self.number) + ')')
-        plt.show()
-
-
-        print("")
-        print("")
-        print("")
-
-
-
-
-
-
-Epochs = 10
-Perceptrons = 10
-
-
-
-ann = ANN(Epochs, 0, Perceptrons)
+# epochs,number,perceptrons
+ann = ANN(1000)
 ann.initialization()
 ann.run()
-
-ann = ANN(Epochs, 1, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 2, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 3, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 4, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 5, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 6, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 7, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 8, Perceptrons)
-ann.initialization()
-ann.run()
-
-ann = ANN(Epochs, 9, Perceptrons)
-ann.initialization()
-ann.run()
-
